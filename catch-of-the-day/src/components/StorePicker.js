ae33dba134c895cc5f29c382f0c09b19fefab4c8
AnnaKolisz <annakolisz@10g.pl>
2020-05-15 18:49:31 +0200

    lessons 1-4

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import { formatPrice } from '../helpers';
+
+class StorePicker extends React.Component {
+    render() {
+        return (
+            <Fragment>
+                <form className="store-selector">
+                    <h2>Please Enter a Store</h2>
+                    <input type="text" required placeholder="Store Name" />
+
+                    <button type="submit">Visit Store -></button>
+                </form>
+            </Fragment>
+        );
+
+     
+    }
+}
+
+export default StorePicker;
+
